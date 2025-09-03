f01e20e545b06626e3bc4cb9e10cb1dfc77488b7
Nagamasa <68956085+davismaulana@users.noreply.github.com>
2025-09-03 16:13:11 +0700

    fix(Hero): Adjust vertical gradient line positioning
    Repositions the vertical gradient line on larger screens to align correctly with the adjacent content, addressing a minor layout issue.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -128,7 +128,7 @@ const Hero: React.FC = () => {
           </div>
 
           {/* Vertical gradient line */}
-          <div className="hidden lg:flex absolute top-0 bottom-0 left-1/2 -translate-x-1/2 items-center justify-center pointer-events-none">
+          <div className="hidden lg:flex absolute top-0 bottom-0 left-1/2 -translate-x-1/2 items-center justify-center pointer-events-none -ml-8">
               <div className="relative w-1 h-80">
                   <div 
                       className="absolute inset-0 rounded-full"
